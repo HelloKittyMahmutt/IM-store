2ee09a69f3431ca0f5bb0d9311436efeabf0d370
Iliyaz Mehmed <ilyas100681@gmail.com>
2026-03-29 13:46:52 +0300

    Include favicon in the Head component
    Add favicon link in the Head component for the app.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,1 +1,12 @@
+import Head from 'next/head';
 
+export default function App({ Component, pageProps }) {
+  return (
+    <>
+      <Head>
+        <link rel="icon" href="/favicon.png" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
+}
