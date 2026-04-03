0d021cd90e4d0f7498e17b8663b2428420a7c71c
Iliyaz Mehmed <ilyas100681@gmail.com>
2026-04-03 14:50:38 +0300

    Import AnnouncementBar component in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { AnnouncementBar } from './components/AnnouncementBar';
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import { AnimatePresence } from 'motion/react';
